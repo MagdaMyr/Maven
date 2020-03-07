f357dddb036cebab31df333c12f45c6471ec3fab
Michal Chmielewski <michal.m.chmielewski@gmail.com>
2020-03-07 13:39:21 +0100

    composition

---
+++ src/main/java/oop/composition/Computer.java
@@ -0,0 +1,27 @@
+package oop.composition;
+
+public class Computer {
+
+    private Processor processor;
+    private GraphicCard graphicCard;
+    private PowerSuply powerSuply;
+
+    public Computer(Processor processor, GraphicCard graphicCard,
+                    PowerSuply powerSuply) {
+        this.processor = processor;
+        this.graphicCard = graphicCard;
+        this.powerSuply = powerSuply;
+    }
+
+    public Processor getProcessor() {
+        return processor;
+    }
+
+    public GraphicCard getGraphicCard() {
+        return graphicCard;
+    }
+
+    public PowerSuply getPowerSuply() {
+        return powerSuply;
+    }
+}
---
+++ src/main/java/oop/composition/PowerSuply.java
@@ -0,0 +1,15 @@
+package oop.composition;
+
+public class PowerSuply {
+
+    private String name;
+
+    public PowerSuply(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
