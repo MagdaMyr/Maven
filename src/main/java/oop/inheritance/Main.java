d6068056cd18c7ea20be29365b4b5263a8a2ea10
Michal Chmielewski <michal.m.chmielewski@gmail.com>
2020-03-07 11:45:49 +0100

    Inheritance main

---
+++ src/main/java/oop/inheritance/Main.java
@@ -0,0 +1,14 @@
+package oop.inheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal("name", 0,
+                "race");
+        Cat cat = new Cat("Mruczek", 3, "Syjamski");
+        Dog dog = new Dog("Reksio", 5, "Beagle",
+                "trikolor");
+
+    }
+
+}
