11ad58f8836cec9d8a91d6ded86e5683e0848e76
Michal Chmielewski <michal.m.chmielewski@gmail.com>
2020-03-07 14:39:08 +0100

    fizzbuzz

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,20 @@
+public class FizzBuzz {
+
+    //Wydrukuj liczby od 1 do 100.
+    //Jeśli liczba jest podzielna przez 3 wydrukuj Fizz.
+    //Jeśli liczba jest podzielna przez 5 wydrukuj Buzz.
+    //Jeśli liczba jest podzielna przez 3 oraz 5 wydrukuj FizzBuzz.
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
