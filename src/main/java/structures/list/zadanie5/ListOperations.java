9f59e89b42030e9bfc76c55b0f4b9269be54e29e
Michal Chmielewski <michal.m.chmielewski@gmail.com>
2020-03-08 15:55:11 +0100

    remove dulicates implementation

--- src/main/java/structures/list/zadanie5/ListOperations.java
+++ src/main/java/structures/list/zadanie5/ListOperations.java
@@ -1,11 +1,19 @@
 package structures.list.zadanie5;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ListOperations {
 
+    /*public static List<String> removeDuplicates(List<String> list) {
+        Set<String> set = new LinkedHashSet<>(list);
+        return new ArrayList<>(set);
+    }*/
+
     public static List<String> removeDuplicates(List<String> list) {
-        return null;
+        return list.stream()
+                .distinct()
+                .collect(Collectors.toList());
     }
 
 }
