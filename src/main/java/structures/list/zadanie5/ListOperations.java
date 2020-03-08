f01963d71f836f0a85f3d3e7f594268d63693b3c
Michal Chmielewski <michal.m.chmielewski@gmail.com>
2020-03-08 15:36:26 +0100

    remove dulicates tests

---
+++ src/main/java/structures/list/zadanie5/ListOperations.java
@@ -0,0 +1,11 @@
+package structures.list.zadanie5;
+
+import java.util.List;
+
+public class ListOperations {
+
+    public static List<String> removeDuplicates(List<String> list) {
+        return null;
+    }
+
+}
